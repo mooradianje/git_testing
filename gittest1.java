faa866ef615f4613e427903e2fc14e228ea40f23
mooradianje <james.e.mooradian@gmail.com>
2016-02-03 23:21:23 -0500

    added a bunch of text explaining how to use git

--- gittest1.java
+++ gittest1.java
@@ -1,3 +1,39 @@
+/* this will just be a file that reminds the procedure 
+for using git on the cloud.*/
+/*
+1:  git config --global user.email "[email@address]"
+2:  git config --global user.name "[username]"
+^^the above allows git to work online
+
+TO GET A REPO:
+git clone https://github.com/[username]/[gitrepo (no.git)] 
+
+repo should now be on local drive (cd /[gitrepo])
+cd [gitrepo]
+
+you can check the BRANCH with:
+git branch
+
+you can STAGE and COMMIT each file with the following
+git add [filename.ext]
+git commit -m "this is a message for when you upload a change"
+
+this only does it on the LOCAL side of things - 
+changes will be stored with the following command:
+git push origin master
+^enter your username & password after this, check github
+
+git log
+^this lists a log of changes made in the structure
+
+git status
+^this gives you the status of changed files (in progress)
+
+git checkout -- [filename.ext]
+^this restores the last backup for git
+
+
+
 public class gittest1 {
     
     public static void main(String[] args) {
